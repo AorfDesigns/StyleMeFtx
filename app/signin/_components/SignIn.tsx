d4945ceaa772c67f5c2cb19a3736f5627043b164
aorfdesigns <rahformatives@gmail.com>
2024-09-23 11:39:16 +0100

    Made changes

--- app/signin/_components/SignIn.tsx
+++ app/signin/_components/SignIn.tsx
@@ -45,7 +45,7 @@ const SignInForm: FC = () => {
             className="w-full flex items-center justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             <SiGoogle className="w-5 h-5 mr-2" />
-            Sign in with Google
+            Log in with Google
           </button>
         </div>
       </div>
